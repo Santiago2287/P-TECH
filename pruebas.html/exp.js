ca9f05ba988b6e838f2a7d5f43872249262c4498
El pepe <santiagonano13@gmail.com>
2023-04-25 16:24:43 -0600

    Nueva carpeta, prototipo de santiago

--- pruebas.html/exp.js
+++
@@ -1,6 +0,0 @@
-const slider = document.querySelector('.slider');
-
-slider.addEventListener('input', () => {
-  const value = slider.value;
-  slider.style.background = `linear-gradient(to right, #4CAF50 0%, #4CAF50 ${value}%, #ccc ${value}%, #ccc 100%)`;
-});
